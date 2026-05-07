5c69aa3919e37be1819b84bae27f4089b72ad41e
Gautam Shah <meetgautzzz@gmail.com>
2026-05-07 16:35:54 +0530

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,11 +13,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-bg text-text-primary min-h-screen">
-        {children}
-        <CookieConsent />
-      </body>
-    </html>
-  );
+    return (
+  <html lang="en">
+
+    <head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-40DTYGNHTK');
+          `,
+        }}
+      />
+    </head>
+
+    <body className="bg-bg text-text-primary min-h-screen">
+      {children}
+      <CookieConsent />
+    </body>
+  </html>
+);
 }
